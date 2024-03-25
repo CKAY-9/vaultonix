c7baa51177b4c5e526344cc06b5ce5d9084bccbf
CKAY-9 <foxtrotcyanide@gmail.com>
2024-03-25 13:52:06 -0700

    No commit message.

--- vaultonix-backend/src/main.ts
+++ vaultonix-backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { existsSync, readFileSync } from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  let http_options = {};
+  if (existsSync("./key.pem") && existsSync("./cert.pem")) {
+    http_options = {
+      key: readFileSync("./key.pem"),
+      cert: readFileSync("./cert.pem")
+    };
+  }
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: http_options
+  });
   app.enableCors();
   await app.listen(3001);
 }
